1bba40a4d386789b99f12d4ecb1c39dacb616d48
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-09-28 18:56:14 +0530

    Updated the analytics section

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom'
 import Stairs from './components/common/Stairs.jsx'
 import NavContext from './context/NavContext.jsx'
 import { inject } from '@vercel/analytics';
-
+import { Analytics } from "@vercel/analytics/react"
 if (import.meta.env.PROD) {
   inject();
 }
@@ -22,4 +22,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </Stairs>
     </HashRouter>
   </React.StrictMode>,
+  <Analytics />
 )
