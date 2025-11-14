ab321167b9de1e392be9374588abf85baa1a32c2
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-11-14 14:17:38 +0530

    Updated navbar

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -204,7 +204,7 @@ const FullScreenNav = () => {
                             <div className='moveX flex items-center'>
                                 <h2 className='whitespace-nowrap font-space-grotesk lg:text-[8vw] text-5xl  text-center lg:leading-[0.8] lg:pt-10 pt-4 uppercase'>Meet Our Team</h2>
                                 <img className='lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover' src='https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg' alt='' />
-                                <h2 className='whitespace-nowrap font-space-grotesk lg:text-[8vw] text-5xl  text-center lg:leading-[0.8] lg:pt-10 pt-4 uppercase'>The NEXTGEN Family</h2>
+                                <h2 className='whitespace-nowrap font-space-grotesk lg:text-[8vw] text-5xl  text-center lg:leading-[0.8] lg:pt-10 pt-4 uppercase'>NEXTGEN Family</h2>
                                 <img className='lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover' src='https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg' alt='' />
                             </div>
                             <div className='moveX flex items-center'>
