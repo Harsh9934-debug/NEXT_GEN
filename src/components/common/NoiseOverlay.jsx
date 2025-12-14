715563df395e7ff433e27ab55407b27b9e0e2fb0
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-12-14 11:46:25 +0530

    refactor: Simplify `App.jsx` routing by removing `AnimatePresence` and `PageWrapper`, integrate `ScrollToTop`, and add responsive display to `NoiseOverlay`.

--- src/components/common/NoiseOverlay.jsx
+++ src/components/common/NoiseOverlay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NoiseOverlay = () => {
     return (
-        <div className="pointer-events-none fixed inset-0 z-50 opacity-[0.03]">
+        <div className="hidden md:block pointer-events-none fixed inset-0 z-50 opacity-[0.03]">
             <div
                 className="absolute inset-0 bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-40 brightness-100 contrast-150"
                 style={{
--- src/components/common/ScrollToTop.jsx
+++ src/components/common/ScrollToTop.jsx
@@ -2,13 +2,13 @@ import { useLayoutEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 const ScrollToTop = () => {
-  const location = useLocation()
+    const location = useLocation()
 
-  useLayoutEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
-  }, [location.pathname])
+    useLayoutEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+    }, [location.pathname])
 
-  return null
+    return null
 }
 
 export default ScrollToTop
